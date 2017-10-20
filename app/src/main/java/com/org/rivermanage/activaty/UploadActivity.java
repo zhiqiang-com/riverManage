bf7665a3a5bad2c714ad58b66370ef1561fed192
苟云瑞 <1743037097@qq.com>
2017-10-20 08:59:10 +0800

    11

--- app/src/main/java/com/org/rivermanage/activaty/UploadActivity.java
+++ app/src/main/java/com/org/rivermanage/activaty/UploadActivity.java
@@ -1,5 +1,6 @@
 package com.org.rivermanage.activaty;
 
+import android.content.Context;
 import android.content.Intent;
 import android.database.Cursor;
 import android.graphics.BitmapFactory;
@@ -12,6 +13,7 @@ import android.view.View;
 import android.widget.Button;
 import android.widget.ImageButton;
 import android.widget.ImageView;
+import android.widget.RelativeLayout;
 import android.widget.TextView;
 
 import com.baidu.location.BDAbstractLocationListener;
@@ -20,12 +22,40 @@ import com.baidu.location.Poi;
 import com.org.rivermanage.R;
 import com.org.rivermanage.base.LocationApplication;
 import com.org.rivermanage.service.LocationService;
+import com.org.rivermanage.utils.AudioRecoderUtils;
+import com.org.rivermanage.utils.PopupWindowFactory;
+import com.org.rivermanage.utils.TimeUtils;
+
+import android.Manifest;
+import android.app.Activity;
+import android.content.Context;
+import android.content.pm.PackageManager;
+import android.support.v4.app.ActivityCompat;
+import android.support.v4.content.ContextCompat;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.util.DisplayMetrics;
+import android.view.Gravity;
+import android.view.MotionEvent;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageView;
+import android.widget.RelativeLayout;
+import android.widget.TextView;
+import android.widget.Toast;
 
 import java.sql.SQLOutput;
 
 public class UploadActivity extends AppCompatActivity {
+    static final int VOICE_REQUEST_CODE = 66;
 
-
+    private Button mButton;
+    private ImageView mImageView;
+    private TextView mTextView;
+    private AudioRecoderUtils mAudioRecoderUtils;
+    private Context context;
+    private PopupWindowFactory mPop;
+    private RelativeLayout rl;
         private LocationService locationService;
         private TextView LocationResult;
         private Button Refresh;
@@ -37,6 +67,47 @@ public class UploadActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_upload);
+
+
+        context = this;
+
+        rl = (RelativeLayout) findViewById(R.id.r1);
+
+        mButton = (Button) findViewById(R.id.button);
+
+        //PopupWindow的布局文件
+        final View view = View.inflate(this, R.layout.layout_microphone, null);
+
+        mPop = new PopupWindowFactory(this,view);
+
+        //PopupWindow布局文件里面的控件
+        mImageView = (ImageView) view.findViewById(R.id.iv_recording_icon);
+        mTextView = (TextView) view.findViewById(R.id.tv_recording_time);
+
+        mAudioRecoderUtils = new AudioRecoderUtils();
+
+        //录音回调
+        mAudioRecoderUtils.setOnAudioStatusUpdateListener(new AudioRecoderUtils.OnAudioStatusUpdateListener() {
+
+            //录音中....db为声音分贝，time为录音时长
+            @Override
+            public void onUpdate(double db, long time) {
+                mImageView.getDrawable().setLevel((int) (3000 + 6000 * db / 100));
+                mTextView.setText(TimeUtils.long2String(time));
+            }
+
+            //录音结束，filePath为保存路径
+            @Override
+            public void onStop(String filePath) {
+                Toast.makeText(UploadActivity.this, "录音保存在：" + filePath, Toast.LENGTH_SHORT).show();
+                mTextView.setText(TimeUtils.long2String(0));
+            }
+        });
+        //6.0以上需要权限申请
+        requestPermissions();
+
+
+
         LocationResult = (TextView) findViewById(R.id.tv_location);
         UpImage = (ImageView) findViewById(R.id.tv_im);
 
@@ -48,6 +119,67 @@ public class UploadActivity extends AppCompatActivity {
         UpImage.setOnClickListener(new addPhoto());
 
     }
+
+    private void requestPermissions() {
+        //判断是否开启摄像头权限
+        if ((ContextCompat.checkSelfPermission(context,
+                Manifest.permission.WRITE_EXTERNAL_STORAGE) == PackageManager.PERMISSION_GRANTED) &&
+                (ContextCompat.checkSelfPermission(context,
+                        Manifest.permission.RECORD_AUDIO) == PackageManager.PERMISSION_GRANTED)
+                ) {
+            StartListener();
+
+            //判断是否开启语音权限
+        } else {
+            //请求获取摄像头权限
+            ActivityCompat.requestPermissions((Activity) context,
+                    new String[]{Manifest.permission.WRITE_EXTERNAL_STORAGE, Manifest.permission.RECORD_AUDIO}, VOICE_REQUEST_CODE);
+        }
+    }
+    public void onRequestPermissionsResult(int requestCode, String[] permissions, int[] grantResults) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults);
+
+        if (requestCode == VOICE_REQUEST_CODE) {
+            if ((grantResults[0] == PackageManager.PERMISSION_GRANTED) && (grantResults[1] == PackageManager.PERMISSION_GRANTED) ) {
+                StartListener();
+            } else {
+                Toast.makeText(context, "已拒绝权限！", Toast.LENGTH_SHORT).show();
+            }
+        }
+    }
+    public void StartListener(){
+        //Button的touch监听
+        mButton.setOnTouchListener(new View.OnTouchListener() {
+            @Override
+            public boolean onTouch(View v, MotionEvent event) {
+
+                switch (event.getAction()){
+
+                    case MotionEvent.ACTION_DOWN:
+
+                        mPop.showAtLocation(rl, Gravity.CENTER, 0, 0);
+
+                        mButton.setText("松开保存");
+                        mAudioRecoderUtils.startRecord();
+
+
+                        break;
+
+                    case MotionEvent.ACTION_UP:
+
+                        mAudioRecoderUtils.stopRecord();        //结束录音（保存录音文件）
+//                        mAudioRecoderUtils.cancelRecord();    //取消录音（不保存录音文件）
+                        mPop.dismiss();
+                        mButton.setText("按住说话");
+
+                        break;
+                }
+                return true;
+            }
+        });
+    }
+
+
 
     //拉取相册
     private class addPhoto implements View.OnClickListener {
